4c0c4fabdebc58de08bfb8fd3df0ee880f816374
Venus Teah <venus.teah@gmail.com>
2022-02-24 11:48:12 -0500

    Created Category and Quiz Model

---
+++ quiz-app/src/main/java/com/project/quizapp/model/category.java
@@ -0,0 +1,26 @@
+package com.project.quizapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+
+import javax.persistence.*;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class category {
+
+    //FIELDS
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotEmpty
+    private String categoryName;
+
+
+}
