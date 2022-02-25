9b09f69b461ffe6c22fc6bb8ab958e913024a740
Venus Teah <venus.teah@gmail.com>
2022-02-24 21:10:44 -0500

    Created repo for Category

---
+++ quiz-app/src/main/java/com/project/quizapp/respository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.project.quizapp.respository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Locale;
+
+public class CategoryRepository extends JpaRepository<Category, Long> {
+
+}
