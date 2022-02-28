4c96580ccf569ec26e6fa5d210a2f28b3c526240
Venus Teah <venus.teah@gmail.com>
2022-02-27 20:29:15 -0500

    fixed optional error with post by id method

--- quiz-app/src/main/java/com/project/quizapp/respository/QuizRepository.java
+++ quiz-app/src/main/java/com/project/quizapp/respository/QuizRepository.java
@@ -9,4 +9,5 @@ public interface QuizRepository extends JpaRepository<Quiz, Long> {
 
     Quiz findByNameIgnoreCase(String name);
 
+
 }
--- quiz-app/src/main/java/com/project/quizapp/service/QuizService.java
+++ quiz-app/src/main/java/com/project/quizapp/service/QuizService.java
@@ -1,5 +1,6 @@
 package com.project.quizapp.service;
 
+import com.project.quizapp.exceptions.InformationExistsException;
 import com.project.quizapp.exceptions.InformationNotFoundException;
 import com.project.quizapp.model.Category;
 import com.project.quizapp.model.Quiz;
@@ -18,7 +19,50 @@ public class QuizService {
     private QuizRepository quizRepo;
     private CategoryRepository categoryRepo;
 
-    //GET METHODS
+
+//POST METHODS
+
+//    GETQUIZ() METHOD USED IN POST METHOD TO STOP DUPLICATE CODE
+    public Quiz getQuiz(Quiz quizObject, Category category) {
+        Quiz quiz = quizRepo.findByNameIgnoreCase(quizObject.getQuizName());
+        if (quiz != null) {
+            throw new InformationExistsException("Quiz with the name " + quiz.getQuizName() + " already exists.");
+        }
+        quizObject.setCategory(category);
+        return quizRepo.save(quizObject);
+    }
+
+
+
+
+
+    //POST QUIZ BY CATEGORY ID
+    public Quiz createQuizByCategoryId(Long categoryId, Quiz quizObject) {
+
+
+        //Find by ID throws back optional, and you can't set the regular category,
+        // so to get around this I will use .or else throw method which returns regular category
+        Category category = categoryRepo.findById(categoryId).orElseThrow(()-> new InformationNotFoundException(
+                "Category with ID " + categoryId + " does not exist. Please try a different Category ID."));
+//        if (!category.isPresent()) {
+//            throw new InformationNotFoundException(
+//                    "Category with ID " + categoryId + " does not exist. Please try a different Category ID.");
+//        }
+//        Quiz quiz = quizRepo.findByNameIgnoreCase(quizObject.getQuizName());
+//        if (quiz != null) {
+//            throw new InformationExistsException("Quiz with the name " + quiz.getQuizName() + " already exists.");
+//        }
+//        quizObject.setCategory(category);
+//        return quizRepo.save(quizObject);
+
+        return getQuiz(quizObject, category);
+    }
+
+
+
+
+
+//GET METHODS
 
     //GET ALL QUIZZES
     public List<Quiz> getAllQuizzes() {
