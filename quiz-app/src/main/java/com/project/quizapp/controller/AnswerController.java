9b29dc5d5ea2218f4032d9ed2603f41f862a1af8
Venus Teah <venus.teah@gmail.com>
2022-02-27 23:01:27 -0500

    Created answer controller

---
+++ quiz-app/src/main/java/com/project/quizapp/controller/AnswerController.java
@@ -0,0 +1,37 @@
+package com.project.quizapp.controller;
+
+
+import com.project.quizapp.model.Answer;
+import com.project.quizapp.service.AnswerService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping(path ="/api")
+public class AnswerController {
+    //DEPENDENCIES
+    AnswerService answerService;
+
+    //GET MAPPING
+    //GET Answers BY CATEGORY
+    @GetMapping("question/{questionId}/answers")
+    public List<Answer> getAllAnswersByQuestionId(@PathVariable(value="questionId") Long questionId){
+        return answerService.getAllAnswerByQuestionId(questionId);
+    }
+
+    //GET ALL QUIZZES
+    @GetMapping("answer/all")
+    public List<Answer> getAllAnswer(){
+        return answerService.getAllAnswers();
+    }
+
+    //POST MAPPING
+    //POST BY CATEGORY ID
+    @PostMapping("question/{questionId}/answer/new")
+    public Answer createAnswerByQuestionId(@PathVariable(value="questionId") Long questionId, @RequestBody Answer answerObject){
+        return answerService.createAnswerByQuestionId(questionId, answerObject);
+    }
+}
