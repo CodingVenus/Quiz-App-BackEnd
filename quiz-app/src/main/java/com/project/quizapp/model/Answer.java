ac315976bff30b8ef7616b10f002a8cc095e5ab2
Venus Teah <venus.teah@gmail.com>
2022-02-27 19:05:37 -0500

    mapped answer to question

--- quiz-app/src/main/java/com/project/quizapp/model/Answer.java
+++ quiz-app/src/main/java/com/project/quizapp/model/Answer.java
@@ -1,23 +1,31 @@
 package com.project.quizapp.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 public class Answer {
+
+    //FIELDS
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String answer;
     private boolean isCorrect;
+
+    //MAPPING TO QUESTION
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name= "question_id")
+    private Question question;
+
+
 }
